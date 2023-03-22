a25b19b68d0b64218b4277fef5ec9ee6f64dd593
Prakhar Trivedi <53103894+Prakhar896@users.noreply.github.com>
2023-03-22 11:33:37 +0800

    Initial Commit

---
+++ HabitHobbit/HabitHobbitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitHobbitApp.swift
+//  HabitHobbit
+//
+//  Created by Prakhar Trivedi on 22/3/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitHobbitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
